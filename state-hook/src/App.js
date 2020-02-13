1455ae7c2ed0c472b038e5a1561146d443fea0d8
Matheus Santos <matheus.santos@ilegra.com>
2020-02-13 14:43:13 -0300

    Added useState with previous state

--- state-hook/src/App.js
+++ state-hook/src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './App.css';
 import ClassCounter from './components/ClassCounter';
 import HookCounter from './components/HookCounter';
+import HookCounterTwo from './components/HookCounterTwo';
+import ClassCounterTwo from './components/ClassCounterTwo';
 
 function App() {
   return (
     <div className="App">
+      <HookCounterTwo></HookCounterTwo>
+      {/* <ClassCounterTwo></ClassCounterTwo> */}
       {/* <ClassCounter></ClassCounter> */}
-      <HookCounter></HookCounter>
+      {/* <HookCounter></HookCounter> */}
     </div>
   );
 }
