24210a3fb70bf007ecc054ea930695d6265a1fe2
Matheus Santos <matheus.santos@ilegra.com>
2020-02-17 14:42:07 -0300

    Added fetching data with useEffect

--- state-hook/src/App.js
+++ state-hook/src/App.js
@@ -13,12 +13,14 @@ import HookMouse from './components/HookMouse';
 import MouseContainer from './components/MouseContainer';
 import IntervalClassCounter from './components/IntervalClassCounter';
 import IntervalHookCounter from './components/IntervalHookCounter';
+import DataFetching from './components/DataFetching';
 
 function App() {
   return (
     <div className="App">
-      <IntervalClassCounter></IntervalClassCounter>
-      <IntervalHookCounter></IntervalHookCounter>
+      <DataFetching></DataFetching>
+      {/* <IntervalClassCounter></IntervalClassCounter> */}
+      {/* <IntervalHookCounter></IntervalHookCounter> */}
       {/* <ClassMouse></ClassMouse> */}
       {/* <HookMouse></HookMouse> */}
       {/* <MouseContainer></MouseContainer> */}
